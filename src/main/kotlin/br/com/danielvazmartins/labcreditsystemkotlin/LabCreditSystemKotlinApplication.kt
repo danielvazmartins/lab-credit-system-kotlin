977fdd079654d096754f86280edabe50731110ef
Daniel Vaz Villalobos Martins <danielvazmartins@gmail.com>
2023-10-24 11:57:06 -0300

    criando projeto com o spring boot initializr

---
+++ src/main/kotlin/br/com/danielvazmartins/labcreditsystemkotlin/LabCreditSystemKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.danielvazmartins.labcreditsystemkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabCreditSystemKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LabCreditSystemKotlinApplication>(*args)
+}
